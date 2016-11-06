bf6157e16f942699be750dd8235ba8292c67a4dd
Hakim Cassimally <hakim.cassimally@gmail.com>
2016-11-06 13:04:45 +0000

    findWordInLine

--- s4/words/src/Lib.hs
+++ s4/words/src/Lib.hs
@@ -1,9 +1,21 @@
 module Lib
     ( Grid
     , formatGrid
+    , findWord
+    , findWordInLine
     ) where
+
+import Data.List (isInfixOf)
 
 type Grid = [String]
 
 formatGrid :: Grid -> String
 formatGrid = unlines
+
+findWord :: Grid -> String -> Bool
+findWord = undefined
+
+findWordInLine :: String -> String -> Bool
+-- findWordInLine word line = word `isInfixOf` line
+-- findWordInLine word line = isInfixOf word line
+findWordInLine = isInfixOf
