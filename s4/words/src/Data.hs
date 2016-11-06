90d00933ba4abcf2983aefd5c79e7de6af0b8681
Hakim Cassimally <hakim.cassimally@gmail.com>
2016-11-06 14:40:23 +0000

    Basic tests

---
+++ s4/words/src/Data.hs
@@ -0,0 +1,27 @@
+module Data (grid, languages) where
+
+grid = [ "__C________R___"
+       , "__SI________U__"
+       , "__HASKELL____B_"
+       , "__A__A_____S__Y"
+       , "__R___B___C____"
+       , "__PHP____H_____"
+       , "____S_LREP_____"
+       , "____I__M_Y__L__"
+       , "____L_E__T_O___"
+       , "_________HB____"
+       , "_________O_____"
+       , "________CN_____"
+       ]
+
+languages = [ "BASIC"
+            , "COBOL"
+            , "CSHARP"
+            , "HASKELL"
+            , "LISP"
+            , "PERL"
+            , "PHP"
+            , "PYTHON"
+            , "RUBY"
+            , "SCHEME"
+            ]
--- s4/words/test/Spec.hs
+++ s4/words/test/Spec.hs
@@ -1,2 +1,19 @@
+import Test.Hspec
+import Lib
+import Data
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  describe "formatGrid" $ do
+    it "joins up a grid into a string" $ do
+      formatGrid ["abc", "def", "ghi"] `shouldBe` "abc\ndef\nghi\n"
+
+  describe "findWord" $ do
+    it "finds a word on the grid" $ do
+      findWord grid "HASKELL" `shouldBe` Just "HASKELL"
+      findWord grid "PERL" `shouldBe` Just "PERL"
+      findWord grid "FRENCH" `shouldBe` Nothing
+
+  describe "findWords" $ do
+    it "should find all the words in word-list" $ do
+      findWords grid languages `shouldBe` languages
