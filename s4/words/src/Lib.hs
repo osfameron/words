4f323dab14dd312018090603ff882e0a2f1d9613
Hakim Cassimally <hakim.cassimally@gmail.com>
2016-11-06 13:21:44 +0000

    findWords using Maybe & catMaybes

--- s4/words/src/Lib.hs
+++ s4/words/src/Lib.hs
@@ -7,21 +7,24 @@ module Lib
     ) where
 
 import Data.List (isInfixOf)
+import Data.Maybe (catMaybes)
 
 type Grid = [String]
 
 formatGrid :: Grid -> String
 formatGrid = unlines
 
--- findWords :: Grid -> [String] -> [String]
+findWords :: Grid -> [String] -> [String]
 findWords grid words =
   let findWord' = findWord grid
-  in map findWord' words
+      foundWords = map findWord' words
+  in catMaybes foundWords
 
-findWord :: Grid -> String -> Bool
+findWord :: Grid -> String -> Maybe String
 findWord grid word =
   let lines = grid ++ (map reverse grid)
-  in or $ map (findWordInLine word) lines
+      foundWord = or $ map (findWordInLine word) lines
+  in if foundWord then Just word else Nothing
 
 findWordInLine :: String -> String -> Bool
 -- findWordInLine word line = word `isInfixOf` line
